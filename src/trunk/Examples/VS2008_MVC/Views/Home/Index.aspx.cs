8d52eab97b762456fa2d4f3435080aa651dbdf30
serkanesque <56206515+serkanesque@users.noreply.github.com>
2020-04-05 20:47:56 +0100

    1

---
+++ src/trunk/Examples/VS2008_MVC/Views/Home/Index.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UFrame2008.Views.Home
+{
+    public partial class Index : ViewPage
+    {
+        public void DoSomething()
+        {
+            
+        }
+    }
+}
