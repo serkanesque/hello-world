98959ee81781fe52c89a0221749b1ebe30555656
serkanesque <56206515+serkanesque@users.noreply.github.com>
2023-05-09 17:58:13 +0100

    2

---
+++ chantry/script.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector('.sidebar');
+const targetElements = document.querySelectorAll('.target-element');
+const links = document.querySelectorAll('.sidebar .link');
+
+window.addEventListener('scroll', () => {
+
+});
+
+
